ff57467aa2f003678424063c14378bb96d607619
leonguevara <leon.guevara@outlook.com>
2016-04-05 09:18:20 -0500

    Commit inicial

---
+++ MyMusicPlayer/MusicPlayerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MusicPlayerViewController.swift
+//  MyMusicPlayer
+//
+//  Created by León Felipe Guevara Chávez on 2016-04-05.
+//  Copyright © 2016 León Felipe Guevara Chávez. All rights reserved.
+//
+
+import UIKit
+
+class MusicPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
